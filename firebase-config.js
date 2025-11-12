a9eaf5828a125e419920273bcdc24c8bf712d140
bwanking <84254991+bwanking@users.noreply.github.com>
2025-11-12 07:36:46 -0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBRmzT3K-zeia4CMNNStbUiWZgzAPc30FU",
+  authDomain: "asa-db-994cf.firebaseapp.com",
+  projectId: "asa-db-994cf",
+  storageBucket: "asa-db-994cf.firebasestorageapp.com",
+  messagingSenderId: "298756488035",
+  appId: "1:298756488035:web:8090bc025f245e355d3156"
+};
+
+firebase.initializeApp(firebaseConfig);
