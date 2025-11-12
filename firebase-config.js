dedf626045e9cebefe7940614b8c1b31405faf89
bwanking <84254991+bwanking@users.noreply.github.com>
2025-11-12 07:58:04 -0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,19 @@
+// firebase-config.js
+
+// Initialize Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBRmzT3K-zeia4CMNNStbUiWZgzAPc30FU",
   authDomain: "asa-db-994cf.firebaseapp.com",
   projectId: "asa-db-994cf",
-  storageBucket: "asa-db-994cf.firebasestorageapp.com",
+  storageBucket: "asa-db-994cf.appspot.com",
   messagingSenderId: "298756488035",
   appId: "1:298756488035:web:8090bc025f245e355d3156"
 };
 
 firebase.initializeApp(firebaseConfig);
+
+// Firebase services
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+console.log("Firebase initialized:", firebase.app().name);
